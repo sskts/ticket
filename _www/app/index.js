a60cf66319b1fad4b7e41a0526b50c4b3f2d425e
Akito Hataguchi <akito.1221.62@gmail.com>
2016-12-19 00:32:07 +0900

    SASAKI_TICKET-34 angular cli導入

--- _www/app/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-const app_module_1 = require('./app.module');
-// import { environment } from '../environments/environment';
-// if (environment.production) {
-//   enableProdMode();
-// }
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
--- _www/environments/environment.dev.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  motiopicture_api_end_point: ''
-
-};
