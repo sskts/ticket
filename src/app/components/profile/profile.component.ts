d50d9d06a30f21dd1d3620127f7dc03b314db7aa
akito hataguchi <hataguchi@motionpicture.jp>
2017-09-18 18:10:50 +0900

    SSKTS-564 buildスクリプト変更

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -4,6 +4,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { UserService } from '../../service/user/user.service';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -14,27 +16,30 @@ export class ProfileComponent implements OnInit {
   public updateForm: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private user: UserService
   ) {
     console.log('LoginComponent constructor');
   }
 
   public ngOnInit() {
     this.updateForm = this.formBuilder.group({
-      mail: new FormControl('', [
+      mail: new FormControl(this.user.contacts.email, [
         Validators.required,
         Validators.maxLength(10),
         Validators.email
       ]),
-      givenName: new FormControl('', [
+      givenName: new FormControl(this.user.contacts.givenName, [
         Validators.required,
-        Validators.maxLength(10)
+        Validators.maxLength(10),
+        Validators.pattern(/^[ぁ-ゞー]+$/)
       ]),
-      familyName: new FormControl('', [
+      familyName: new FormControl(this.user.contacts.familyName, [
         Validators.required,
-        Validators.maxLength(10)
+        Validators.maxLength(10),
+        Validators.pattern(/^[ぁ-ゞー]+$/)
       ]),
-      phoneNumber: new FormControl('', [
+      phoneNumber: new FormControl(this.user.contacts.telephone, [
         Validators.required,
         Validators.maxLength(10),
         Validators.pattern(/^[0-9]+$/)
