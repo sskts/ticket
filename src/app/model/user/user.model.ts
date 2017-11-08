e401f8fe6abd1b85aa4907f7be8e568f13491e52
akito hataguchi <hataguchi@motionpicture.jp>
2017-11-08 20:44:29 +0900

    SSKTS-692 スマホアプリへ先行販売追加

--- src/app/model/user/user.model.ts
+++ src/app/model/user/user.model.ts
@@ -7,22 +7,16 @@ export class UserModel {
     private updateAt: string | undefined;
     public contacts: sasaki.factory.person.IContact;
     public creditCards: sasaki.factory.paymentMethod.paymentCard.creditCard.ICheckedCard[];
-    public select: {
-        purchase: { date: string; theater: string; } | undefined;
-    } | undefined;
 
      constructor(args?: any) {
         if (args === undefined) {
             args = {};
         }
         this.updateAt = (args.updateAt === undefined) ? undefined : args.updateAt;
-        this.select = (args.select === undefined) ? { purchase: { date: '', theater: '' } } : args.select;
     }
 
     public convertToRecord() {
-        return {
-            select: (this.select === undefined) ? undefined : this.select
-        };
+        return {};
     }
 
     public isFirst() {
--- src/app/service/purchase/purchase.service.ts
+++ src/app/service/purchase/purchase.service.ts
@@ -7,10 +7,19 @@ import * as sasaki from '@motionpicture/sskts-api-javascript-client';
 import { environment } from '../../../environments/environment';
 
 type IMovieTheater = sasaki.factory.place.movieTheater.IPlaceWithoutScreeningRoom;
+type IIndividualScreeningEvent = sasaki.factory.event.individualScreeningEvent.IEventWithOffer;
+export interface ISchedule {
+    theater: IMovieTheater;
+    schedule: {
+        date: string;
+        individualScreeningEvents: IIndividualScreeningEvent[];
+    }[];
+}
 
 @Injectable()
 export class PurchaseService {
     public movieTheaters: IMovieTheater[];
+    public schedule: ISchedule[];
 
     constructor(
         private jsonp: Jsonp
@@ -49,6 +58,45 @@ export class PurchaseService {
         };
         const response = await this.jsonp.get(url, options).retry(3).toPromise();
         console.log('response', response);
+        if (response.json().error !== null) {
+            throw new Error(response.json().error);
+        }
+
+        return response.json().result;
+    }
+
+    /**
+     * スケジュール取得
+     * @method getSchedule
+     * @returns {Promise<IPlaceWithoutScreeningRoom[]>}
+     */
+    public async getSchedule(args: { biginDate: string; endDate: string; }) {
+        if (this.movieTheaters.length === 0) {
+            try {
+                this.schedule = await this.fitchSchedule(args);
+            } catch (err) {
+                throw err;
+            }
+        }
+
+        return this.schedule;
+    }
+
+    /**
+     * スケジュールをAPI経由で取得
+     * @method fitchISchedule
+     * @returns {Promise<void>}
+     */
+    public async fitchSchedule(args: { biginDate: string; endDate: string; }): Promise<ISchedule[]> {
+        const url = `${environment.ticketingSite}/purchase/performances/getSchedule`;
+        const options = {
+            search: {
+                callback: 'JSONP_CALLBACK',
+                biginDate: args.biginDate,
+                endDate: args.endDate
+            }
+        };
+        const response = await this.jsonp.get(url, options).retry(3).toPromise();
         if (response.json().error !== null) {
             throw new Error(response.json().error);
         }
