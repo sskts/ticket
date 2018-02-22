5d1d88929d65e7a28c66b45639527eadbdd39881
akito hataguchi <hataguchi@motionpicture.jp>
2018-02-22 11:10:05 +0900

    SSKTS-831 [スマフォアプリ]パフォーマンスの時間狂い

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import * as moment from 'moment-timezone';
+
+moment.tz.setDefault('Asia/Tokyo');
+
 if (environment.production) {
   enableProdMode();
 }
