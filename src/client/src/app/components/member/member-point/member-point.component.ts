fa8aa8ba779a9be8fb27bb0a1cd4ad5c94fc2304
akito hataguchi <hataguchi@motionpicture.jp>
2018-03-30 19:36:16 +0900

    SSKTS-879 スマホアプリ会員フロー追加

--- src/client/src/app/components/member/member-point/member-point.component.ts
+++ src/client/src/app/components/member/member-point/member-point.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-member-point',
-  templateUrl: './member-point.component.html',
-  styleUrls: ['./member-point.component.scss']
+    selector: 'app-member-point',
+    templateUrl: './member-point.component.html',
+    styleUrls: ['./member-point.component.scss']
 })
 export class MemberPointComponent implements OnInit {
+    public isLoading: boolean;
+    public config: SwiperOptions;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    public ngOnInit() {
+        this.isLoading = false;
+        this.config = {
+            pagination: '.swiper-pagination',
+            paginationClickable: true,
+            autoHeight: true
+        };
+    }
 
 }
