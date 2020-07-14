d535f0ce3cb033a45fa62d346f7c454f14a7c3dc
Akito Hataguchi <hataguchi@motionpicture.jp>
2020-07-14 12:50:13 +0900

    ### Added  - 作品紹介追加

--- src/client/app/models/cms/schedule.ts
+++ src/client/app/models/cms/schedule.ts
@@ -1,3 +1,29 @@
+export enum System {
+    '2D' = 1,
+    '3D' = 2,
+    '4DX' = 3,
+    '4DX3D' = 4,
+    'IMAX' = 5,
+    'IMAX3D' = 6,
+    'ScreenX' = 7,
+    '4DXSCREEN' = 8,
+    'None' = 99,
+}
+
+export enum Sound {
+    'BESTIA' = 1,
+    'Dts-X' = 2,
+    'Dolbyatmos' = 3,
+    'GDC' = 4,
+    'None' = 99,
+}
+
+export enum Voice {
+    'Subtitles' = 1,
+    'Dubbing' = 2,
+    'None' = 3,
+}
+
 export interface ISchedule {
     id: number;
     start_date: string;
@@ -16,9 +42,12 @@ export interface ITheater {
 }
 
 export interface IFormat {
-    system: string;
-    sound: string;
-    voice: string;
+    system_id: System;
+    system_name: string;
+    sound_id: Sound;
+    sound_name: string;
+    voice_id: Voice;
+    voice_name: string;
 }
 
 export interface ITitle {
